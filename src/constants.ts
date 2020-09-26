52353dc7439e62c05fc38a00aa9dc2d4db9fee9d
MariusCasvean <marius.casvean@yahoo.com>
2020-09-26 20:39:30 +0300

    Header in progress

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+// texts, labels, names
+
+const SITE_NAME: string = 'RelaxyTime';
+const MALE_OWNER: string = 'Marius Casvean';
+const FEMALE_OWNER: string = 'Ramona Casvean';
+const MOTO: string = 'We all need some time to relax';
+
+// style colors
+
+const MAIN_BLUE: string = '#33d6ff';
